0440265f354a6603646be4e51c304df978c5a6ee
bluehue22 <61100686+bluehue22@users.noreply.github.com>
2021-01-27 15:40:32 -0500

    Update firemaze.py

--- firemaze.py
+++ firemaze.py
@@ -4,8 +4,8 @@
 
 # Problem 1: Insert maze-defining function
 
-# Problem 2: 
+# Problem 2: Write DFS algorithm, generate 'obstacle density p' vs 'probability that S can be reached from G' plot
 
-# Problem 3: 
+# Problem 3: Write BFS and A* algorithms, generate avg '# nodes explored by BFS - # nodes explored by A*' vs 'obstacle density p' plot
 
 #
